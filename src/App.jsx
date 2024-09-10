f3af7b1b4aa67a9f93c8b9c0e585436f1028ef12
hexaalif <shadullahsad9@gmail.com>
2024-09-11 00:27:24 +0600

    add to do, remove todo with redux toolkit done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Addtodo from "./Component/Addtodo";
+import Todos from "./Component/Todos";
+
 function App() {
   return (
     <>
-      <h1 className="text-4xl">hello redux toolkit</h1>
+      <div className="w-1/2 mx-auto">
+        <h1 className="text-4xl">hello redux toolkit</h1>
+        <Addtodo />
+        <Todos />
+      </div>
     </>
   );
 }
---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [
+    {
+      id: 1,
+      text: "hello world",
+    },
+  ],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      const { id, newTxt } = action.payload;
+      const todoUpdate = state.todos.find((todo) => todo.id === id);
+      if (todoUpdate) {
+        todoUpdate.text = newTxt;
+      }
+    },
+  },
+});
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
